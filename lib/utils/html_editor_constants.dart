0345cbf6faa887c3ec344cb57ac0ce26dce33851
Cecles <getcecled@gmail.com>
2026-06-22 21:15:55 +0100

    chore: add html_editor_constants.dart

---
+++ lib/utils/html_editor_constants.dart
@@ -0,0 +1,8 @@
+class HtmlEditorConstants {
+  static const String summernoteHtmlAssetPath = 'packages/html_editor_enhanced/assets/summernote.html';
+  static const String jqueryAssetPath = 'packages/html_editor_enhanced/assets/jquery.min.js';
+  static const String summernoteCSSAssetPath = 'packages/html_editor_enhanced/assets/summernote-lite.min.css';
+  static const String summernoteJSAssetPath = 'packages/html_editor_enhanced/assets/summernote-lite.min.js';
+  static const String summernoteFontEOTAssetPath = 'packages/html_editor_enhanced/assets/font/summernote.eot';
+  static const String summernoteFontTTFAssetPath = 'packages/html_editor_enhanced/assets/font/summernote.ttf';
+}
